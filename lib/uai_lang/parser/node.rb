522cb2afe5b6b91c3da03a65da43ec7faf9fb6e9
Sérgio Rodrigues <sergioaugrod@gmail.com>
2017-05-09 00:26:49 -0300

    Creates dinamically attr_reader

--- lib/uai_lang/parser/node.rb
+++ lib/uai_lang/parser/node.rb
@@ -1,9 +1,15 @@
 module UaiLang
   module Parser
     class Node
+      attr_reader :type
+
       def initialize(type, args)
         args.each do |key, value|
           instance_variable_set("@#{key}", value)
+
+          self.class.send(:define_method, key) do
+            instance_variable_get("@#{key}")
+          end
         end
 
         @type = type
